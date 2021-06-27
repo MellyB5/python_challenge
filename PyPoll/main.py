1027e305ebb759d071eb5434d4b3c4c70fe54f33
Mel Benson <nat_mel@hotmail.com>
2021-06-27 17:17:39 +0800

    cm

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,17 @@ import csv
 
 csvpath = os.path.join('Resources', 'election_data.csv')
 
+num_rows = 0
+
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
-    print(csvreader)
+    
+    header = next(csvreader)
+    first_row = next(csvreader)
+    num_rows += 1
+
+    for row in csvreader:
+        num_rows += 1
+    
+    print(f"Total votes: {num_rows}")
     
