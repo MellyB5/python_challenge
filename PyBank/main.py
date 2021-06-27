5900e7ec2cd7cf6fb8e66e25c6678c9dc194fb80
Mel Benson <nat_mel@hotmail.com>
2021-06-27 13:54:44 +0800

    cm

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,27 +5,35 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
+    next(csvreader)
 
 # read header row
-csv_header = next(csvreader)
-print(f"CSV Header: {csv_header}")
+    # csv_header = next(csvreader)
+    # print(f"CSV Header: {csv_header}")
+
+    # lines = len(list(csvreader))
+    # print("Total months")
+    # print(lines)
+
+
 
 # count the number of rows
-num_rows = 0
+    num_rows = 0
 
-for row in open(csvpath):
-    num_rows += 1
-    print(f"Total months: {row}")
+    for row in open(csvpath):
+        num_rows += 1
+    print(f"Total months: {num_rows}")
 
 # find the net proft/loss
 sum_pf = 0
 for row in open(csvpath):
-    sum_pf += row[1]
-    print(f"Total: ${sum_pf}")
+    sum_pf += int(row[1])
+print(f"Total: ${sum_pf}")
 
-# find the average
-
+# # find the average, create a list of changes
+# change_list = []
+# for row in open(csvpath):
+#     change_list.append()
 
 # find the max
 
