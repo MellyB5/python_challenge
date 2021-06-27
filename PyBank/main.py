07918d40f1a503c5c0c077939d3441d3995fc715
Mel Benson <nat_mel@hotmail.com>
2021-06-27 12:33:36 +0800

    commit on main

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,10 +4,8 @@ import csv
 csvpath = os.path.join ("Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
-
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
-
 
 # read header row
 csv_header = next(csvreader)
