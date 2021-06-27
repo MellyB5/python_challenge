2eaa3f67453ef3fcfcd4037a53a744cdd67d6d25
Mel Benson <nat_mel@hotmail.com>
2021-06-27 14:23:22 +0800

    cm

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,16 +7,6 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
 
-# read header row
-    # csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
-
-    # lines = len(list(csvreader))
-    # print("Total months")
-    # print(lines)
-
-
-
 # count the number of rows
     num_rows = 0
 
@@ -24,11 +14,21 @@ with open(csvpath) as csvfile:
         num_rows += 1
     print(f"Total months: {num_rows}")
 
-# find the net proft/loss
-sum_pf = 0
+#find the sum of p/f
+pf = []
 for row in open(csvpath):
-    sum_pf += int(row[1])
-print(f"Total: ${sum_pf}")
+    pf.append(row[1])
+    total = sum(pf)
+print("Total $", total)
+
+
+
+
+# find the net proft/loss
+# sum_pf = 0
+# for row in open(csvpath):
+#     sum_pf += int(row[1])
+# print(f"Total: ${sum_pf}")
 
 # # find the average, create a list of changes
 # change_list = []
