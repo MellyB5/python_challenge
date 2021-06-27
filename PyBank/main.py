487818766ec3a88dbefd9c252b4ec242bcc499f9
Mel Benson <nat_mel@hotmail.com>
2021-06-27 16:11:21 +0800

    cm

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,24 +2,44 @@ import os
 import csv
 
 csvpath = os.path.join("Resources", "budget_data.csv")
-
+num_rows = 0
+net_total = 0
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
-
-# count the number of rows
-    num_rows = 0
-
-    for row in open(csvpath):
+    
+    
+    # print(row)
+    header = next(csvreader)
+    first_row = next(csvreader)
+    num_rows += 1
+    net_total += int(first_row[1])
+    for row in csvreader:
         num_rows += 1
-    print(f"Total months: {num_rows}")
+        net_total += int(row[1])
+
+    print(num_rows)
+    print(net_total)
+
+        
+    # count the number of rows
+    # num_rows = 0
+
+    # for row in open(csvpath):
+    #     num_rows += 1
+    # print(f"Total months: {num_rows}")
+
+    # total_net = 0
+    # for row in csvreader:
+    #     total_net += int(row[1])
+    # print(total_net)
 
 #find the sum of p/f
-pf = []
-for row in open(csvpath):
-    pf.append(row[1])
-    total = sum(pf)
-print("Total $", total)
+    # pf = []
+    # for row in open(csvpath):
+    #     pf.append(row[1])
+    # print(pf)
+# total = sum(pf)
+# print("Total $", total)
 
 
 
@@ -28,7 +48,7 @@ print("Total $", total)
 # sum_pf = 0
 # for row in open(csvpath):
 #     sum_pf += int(row[1])
-# print(f"Total: ${sum_pf}")
+# print(f"Total: $, {sum_pf}")
 
 # # find the average, create a list of changes
 # change_list = []
