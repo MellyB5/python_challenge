baed8440ea9baf7ebbe028799059dc4af4b2a6e9
Mel Benson <nat_mel@hotmail.com>
2021-06-27 12:05:05 +0800

    commit on main

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,21 @@
 import os
 import csv
-
-csvpath = os.path.join ("..", "Resources", "budget_data.csv")
+# 
+csvpath = os.path.join ("Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
 
+
+# read header row
+csv_header = next(csvreader)
+print(f"CSV Header: {csv_header}")
+
+# count the number of rows
+num_rows = 0
+
+for row in open(csvpath):
+    num_rows += 1
+    print(row)
