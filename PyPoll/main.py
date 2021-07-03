663e5028f0f0fa70c260f171d8feda4e76f99bae
Mel Benson <nat_mel@hotmail.com>
2021-07-03 11:03:59 +0800

    cm

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,21 +2,57 @@ import os
 import csv
 
 csvpath = os.path.join('Resources', 'election_data.csv')
+output_path = os.path.join("analysis", "analysis.txt")
 
 num_rows = 0
+Khan = 0
+Correy = 0
+Li = 0
+OTooley = 0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
     
     header = next(csvreader)
-    first_row = next(csvreader)
-    num_rows += 1
+    # first_row = next(csvreader)
+    # num_rows += 1
 
     for row in csvreader:
         num_rows += 1
+        if str(row[2]) == "Khan":
+            Khan += 1
+        elif str(row[2]) == "Correy":
+            Correy += 1
+        elif str(row[2]) == "Li":
+            Li += 1
+        elif str(row[2]) == "O'Tooley":
+            OTooley += 1
     
-    print("Election Results")
-    print("----------------------------")
-    print(f"Total votes: {num_rows}")
-    print("----------------------------")
+
     
+    total_votes = Khan + Correy + Li + OTooley
+    khan_votes = round((Khan/total_votes*100),3)
+    correy_votes = round((Correy/total_votes*100),3)
+    li_votes = round((Li/total_votes*100),3)
+    otooley_votes = round((OTooley/total_votes*100),3)
+  
+
+# print analysis
+    analysis = (f"Election Results"
+    f"\n----------------------------"
+    f"\nTotal votes: {total_votes}"
+    "\n----------------------------"
+    f"\nKhan: {khan_votes}% ({Khan})"
+    f"\nCorrey: {correy_votes}% ({Correy})"
+    f"\nLi: {li_votes}% ({Li})"
+    f"\nO'Tooley: {otooley_votes}% ({OTooley})"
+    "\n----------------------------"
+    "\nWinner: Khan"
+    "\n----------------------------"
+    )
+    print(analysis)
+
+# output analysis
+
+with open(output_path,'w') as text_file:
+    text_file.write(analysis)
--- PyPoll/testing.py
+++ PyPoll/testing.py
@@ -3,30 +3,45 @@ import csv
 
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-# def print_votes(pypoll):
-#     name = str(pypoll[2])
-#     votes = int
 
 Khan = 0
 Correy = 0
 Li = 0
 OTooley = 0
 
+
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile,delimiter=',')
+    csvreader = csv.reader(csvfile)
     
-    header = next(csvreader)
+    # header = next(csvreader)
+    # print(header)
 
     for row in csvfile:
-        if str(row[2]) == "Khan":
+        row_list = row.split(sep=",")
+        # print(row_list)
+        if str(row_list[2][:-1]) == "Khan":
             Khan += 1
-        elif str(row[2]) == "Correy":
+        elif str(row_list[2][:-1]) == "Correy":
             Correy += 1
-        elif str(row[2]) == "Li":
+        elif str(row_list[2][:-1]) == "Li":
             Li += 1
-        elif str(row[2]) == "O'Tooley":
+        elif str(row_list[2][:-1]) == "O'Tooley":
             OTooley += 1
     
+
+
+
     print(Khan)
+    print(Correy)
+    print(Li)
+    print(OTooley)
     total_votes = Khan + Correy + Li + OTooley
     print(total_votes)
+
+    print(f"Khan: {Khan/total_votes*100}% {(Khan)}")
+    print(Correy/total_votes*100)
+    print(Li/total_votes*100)
+    print(OTooley/total_votes*100)
+    print('---------------------------')
+    print(f"Winner: Khan")
+    print("---------------------------")
